af7521935f4ba17417c811e007abe8407544194e
Amimul Ehsan <1805056@ugrad.cse.buet.ac.bd>
2022-04-19 23:06:46 +0600

    Fraud microservices in action.

---
+++ fraud/src/main/java/com/rahi/fraud/FraudCheckService.java
@@ -0,0 +1,29 @@
+package com.rahi.fraud;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@Slf4j
+@Service
+public class FraudCheckService {
+
+    private final FraudCheckRepository fraudCheckRepository;
+
+    public boolean isFraudulentCustomer( Integer customerId ) {
+
+        fraudCheckRepository.save(
+                FraudCheckHistory.builder()
+                        .customerId(customerId)
+                        .isFraudster(false)
+                        .createdAt(LocalDateTime.now())
+                        .build()
+        );
+
+        return false;
+    }
+
+}
