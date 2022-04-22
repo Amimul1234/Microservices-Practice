4bbf1ba99cde1e047118b1b48e7b10f6eb7eca01
Amimul Ehsan <1805056@ugrad.cse.buet.ac.bd>
2022-04-22 21:15:00 +0600

    Rabbit mq message producer configured.

--- notification/src/main/java/com/rahi/notification/NotificationApplication.java
+++ notification/src/main/java/com/rahi/notification/NotificationApplication.java
@@ -1,12 +1,9 @@
 package com.rahi.notification;
 
-import com.rahi.amqp.RabbitMqMessageProducer;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(
         scanBasePackages = {"com.rahi.amqp", "com.rahi.notification"}
@@ -19,11 +16,4 @@ public class NotificationApplication {
         SpringApplication.run(NotificationApplication.class, args);
     }
 
-    @Bean
-    CommandLineRunner commandLineRunner( RabbitMqMessageProducer rabbitMqMessageProducer,
-                                         NotificationConfig notificationConfig ) {
-        return args -> rabbitMqMessageProducer.publish(
-                "foo", notificationConfig.getInternalExchange(),
-                notificationConfig.getInternalNotificationRoutingKey());
-    }
 }
