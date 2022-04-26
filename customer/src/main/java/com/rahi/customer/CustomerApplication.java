b324b7d47ac7ea4816d3bb13bb7cad7233c1b1cf
Amimul Ehsan <1805056@ugrad.cse.buet.ac.bd>
2022-04-27 01:08:15 +0600

    Added variable source file for profiles in kubernetes.

--- customer/src/main/java/com/rahi/customer/CustomerApplication.java
+++ customer/src/main/java/com/rahi/customer/CustomerApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication(
         scanBasePackages = {"com.rahi.amqp", "com.rahi.customer"}
@@ -12,6 +14,9 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients(
         basePackages = {"com.rahi.clients.fraud", "com.rahi.clients.notification"}
 )
+@PropertySources({
+        @PropertySource("classpath:clients-${spring.profiles.active}.properties")
+})
 public class CustomerApplication {
     public static void main( String[] args ) {
         SpringApplication.run(CustomerApplication.class, args);
